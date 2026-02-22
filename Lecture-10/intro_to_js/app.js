1ad1421095d9ece10233665038762a25b57b4610
Samarth Vohra <samarthvohra@Samarths-MacBook-Air.local>
2026-02-22 13:18:51 +0530

    JS lecture 1 code

---
+++ Lecture-10/intro_to_js/app.js
@@ -0,0 +1,23 @@
+// let a = 20.6;
+// console.log(a) //print
+// let b = 30;
+// console.log(b) //print
+
+// console.log( typeof(a) );
+// ----------------------
+
+let str = "sam";
+let str2 = 'vohra';
+let str3 = `maverick`; //backticks (imp)
+
+console.log( str );
+console.log( str2 );
+console.log( str3 );
+console.log( typeof(str) );
+console.log( typeof(str2) );
+console.log( typeof(str3) );
+
+
+
+
+
