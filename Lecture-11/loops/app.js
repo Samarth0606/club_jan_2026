cf2879e56247bea4968f3e3a3c20f2c834cdeb08
Samarth Vohra <samarthvohra@Samarths-MacBook-Air.local>
2026-02-23 20:59:12 +0530

    lecture 11 codes

---
+++ Lecture-11/loops/app.js
@@ -0,0 +1,44 @@
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+// console.log(7);
+// console.log(8);
+// console.log(9);
+// console.log(10);
+
+// for looops = minimum executes for 0 times
+
+// for(let i = 1 ; i <= 10 ; i=i+1){
+//     console.log(i);
+// }
+
+// ------------------------------------
+// initial        condition check    increment
+// for(let i = 1 ;    i <= 10 ;       i++){
+//     console.log(i);
+// }
+
+
+// ------------------------------------
+// while = minimum executes for 0 times
+
+// let i = 1; //initial
+// while(i<=10){ //condition check
+//     console.log(i); // 1 2 3 4 5 6 7 8 9 10
+//     i++; //increment // 2 3 4 5 6 7 8 9 10 11
+// }
+
+// ------------------------------------
+// do-while = minimum executes for 1 time
+
+// let j = 1000;
+let j = 1;
+do{
+    console.log(j); // 1 2 3 4 5 6 7 8 9 10
+    j++; // 2 3 4 5 6 7 8 9 10 11
+}
+while(j<=10) 
+
---
+++ Lecture-11/template_literals/app.js
@@ -0,0 +1,22 @@
+
+
+let naam = "samarth vohra"
+let kaam = "teaching"
+let shaher = "New Delhi"
+
+// output: Hi i am samarth vohra, teaching in New Delhi
+// concatenation + 
+
+
+// not a good way
+// let output = "Hi i am " +  naam + ", " + kaam + " in " + shaher
+// console.log(output);
+
+// good way => string template literals //${}
+
+output = `Hi i am ${naam}, ${kaam}  in ${shaher}`
+console.log(output);
+
+
+
+
