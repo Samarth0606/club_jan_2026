70b4f3750a61bad27a25f0c56d42cda8952e99e9
Samarth Vohra <samarthvohra@Samarths-MacBook-Air.local>
2026-03-02 20:21:41 +0530

    js lecture 13_14 code

---
+++ Lecture-13_14/execution_context/app.js
@@ -0,0 +1,70 @@
+
+//                               PHASE - 2
+// let a = 10;
+// console.log(a);
+
+// -------------------
+
+// 3 ways to declare a variable
+// var a = 1
+// let b = 10
+// const c = 100
+
+// ---------------------
+
+// avoid in future
+// var a = 10;
+// console.log(a);
+
+// // var a = "sam";
+// a = true;
+// console.log(a);
+
+// ---------------------
+//use it often
+// let b = 100;
+// console.log(b);
+
+// // let b = true; 
+// b = true
+// console.log(b);
+
+// ---------------------
+//use it often
+// const c = 10;
+// console.log(c);
+
+// // const c = 300;
+// c = true;
+// console.log(c);
+
+// ------------------------------------------
+
+
+// var a = 10;
+// console.log(a);
+
+// function abc(){
+//     console.log("hello from abc");
+// }
+
+// abc();
+
+
+// ------------------------------------------
+
+
+console.log(a)
+var a  = 10;
+var b = 20;
+console.log(b)
+
+
+vohra();
+function vohra(){
+    console.log(c);
+    var c = 30;
+    var d = 40;
+    console.log(d);
+}
+
---
+++ Lecture-13_14/hoisting/app.js
@@ -0,0 +1,20 @@
+
+
+// //hoisting (normal)
+console.log(a); //undefined
+
+var a = true;
+
+//for let => value unavailable instead of undefined
+// ------------------------------
+
+//hoisting 
+
+console.log(b); // error: cannot access b before initialization
+// let b = true;
+const b = true;
+
+// mcp     DTZ     cep
+// DTZ: time span between mcp and cep where the variables 
+// declareed with let & const are not accessible (err: special error)
+
