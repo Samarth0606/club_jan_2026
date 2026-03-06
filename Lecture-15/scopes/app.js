9db364871bc50f9e4a96ed9fd970596c7f95699a
Samarth Vohra <samarthvohra@Samarths-MacBook-Air.local>
2026-03-06 18:54:29 +0530

    js lecture 15 code

---
+++ Lecture-15/scopes/app.js
@@ -0,0 +1,189 @@
+// var a = 10; //global
+// let b = 20; //script
+// const c = 30; // script
+
+// if(true){
+//     var d = 100; //global
+//     let e = 200; //block
+//     const f = 300; //block
+// }
+
+// function sam(){
+//     var g = 1000; //functional
+//     let h = 2000; //block
+//     const i = 3000; //block
+// }
+// sam();
+
+// ---------------------------
+
+// console.log(a)
+// var a = 10;
+// console.log(a);
+
+// ---------------------------
+
+// console.log(a) //stop here only
+// let a = 10;
+// console.log(a);
+
+// ---------------------------
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// var a = 10;
+// let b = 20;
+// const c = 30;
+
+// amazon
+// 10 20 30
+// 10 error error
+// undefined error error
+// undefined error
+
+
+// ---------------------------
+
+// var a = 100;
+// console.log(a);
+// let b = 20;
+// console.log(b);
+
+// function sam(){
+//     var c = 10;
+//     console.log(c);
+// }
+// sam()
+
+
+// ---------------------------
+
+// var a = 100;
+// console.log(a);
+// let b = 20;
+// console.log(b);
+
+// sam()
+// function sam(){
+//     console.log(c);
+//     var c = 10;
+// }
+
+
+
+// ---------------------------
+
+// var a = 100;
+// console.log(a);
+
+// function sam(){
+//     console.log(a);
+// }
+// sam();
+
+// ---------------------------
+
+// var a = 100;
+// console.log(a);
+
+// function sam(){
+//     console.log(a);
+//     var a = 200;
+// }
+// sam();
+
+// console.log(a);
+
+
+// ---------------------------
+
+// var a = 100;
+// console.log(a);
+
+// function sam(){
+//     let a = 200;
+//     console.log(a);
+// }
+// sam();
+
+// console.log(a);
+
+// ---------------------------
+
+// let a = 100;
+// console.log(a);
+
+// if(true){
+//     let a = 200;
+//     console.log(a);
+// }
+
+// console.log(a);
+
+// ---------------------------
+
+// var a = 100;
+// console.log(a);
+
+// if(true){
+//     var a = 200;
+//     console.log(a);
+// }
+
+// console.log(a);
+
+
+// ---------------------------
+
+// var a = 100;
+// console.log(a);
+
+// if(true){
+//     console.log(a , "1");
+//     let a = 200;
+//     console.log(a , "2");
+// }
+
+// console.log(a);
+
+// ---------------------------
+
+// var a = 100;
+// console.log(a);
+
+// if(true){
+//     console.log(a , "1");
+//     var a = 200;
+//     console.log(a , "2");
+// }
+
+// console.log(a);
+
+// ---------------------------
+
+
+// var a = 100;
+// console.log(a);
+
+// if(true){
+//     console.log(a , "1");
+//     a = 50;
+// }
+
+
+// console.log(a);
+
+// -----------------------------
+
+var a = 100;
+console.log(a);
+
+if(true){
+    console.log(a , "1");
+}
+
+let a = 50;
+
+console.log(a);
