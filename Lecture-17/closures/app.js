556da80097af833e8e199a0c1cf6eb096d8ebee2
Samarth Vohra <samarthvohra@Samarths-MacBook-Air.local>
2026-03-09 21:11:22 +0530

    js lecture 17 code

---
+++ Lecture-17/closures/app.js
@@ -0,0 +1,17 @@
+
+function outer(){
+    console.log("I M OUTER");
+    let a = 10;
+    let b = 20;
+    let c = 30;
+    
+    function inner(){
+        console.log("I M INNER");
+        console.log(a+c);
+    }
+    return inner;
+}
+
+
+let returnedVal = outer();
+returnedVal();
