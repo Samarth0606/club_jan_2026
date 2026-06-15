3aa37264a19c0e1bbb8b2d4a4d0989acb4a6f7b3
Samarth Vohra <samarthvohra@Samarths-MacBook-Air.local>
2026-06-15 19:59:58 +0530

    react 10 code

---
+++ React_10/swiggy_02/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './components/Header'
+import Body from './components/Body'
+import Footer from './components/Footer'
+import ApiCalling from './components/ApiCalling'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ React_10/swiggy_02/src/components/RestaurantCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function RestaurantCard({restArray}) {
+  return (
+    restArray.map((item)=>{
+        return(
+            <div className='m-8 w-58'>
+                <img className='w-58 h-42 rounded-2xl' src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${item.info.cloudinaryImageId}`} alt="" />
+                <h1 className='font-bold'>{item.info.name}</h1>
+                <span className='font-bold'>{item.info.avgRating}</span>
+                <span className=' mx-3'>{item.info.sla.slaString}</span>
+                <p>{item.info.cuisines[0]}</p>
+                <p>{item.info.locality}</p>
+            </div>
+        )
+    })
+  )
+}
+
+export default RestaurantCard
