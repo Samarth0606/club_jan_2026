6728a4c0d16b8dc39616d938b358f86186c76fc2
Samarth Vohra <samarthvohra@Samarths-MacBook-Air.local>
2026-06-15 20:26:29 +0530

    react 10 code

--- React_10/swiggy_02/src/App.jsx
+++ React_10/swiggy_02/src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Header from './components/Header'
 import Body from './components/Body'
 import Footer from './components/Footer'
-import ApiCalling from './components/ApiCalling'
+import { Outlet } from 'react-router-dom'
 
 function App() {
   return (
     <div>
       <Header />
-      <Body />
+      {/* <Body /> */}
+      <Outlet />
       <Footer />
     </div>
   )
